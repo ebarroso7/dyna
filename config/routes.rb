21da228fec9d2c38635b2efe75f27b65e3e13f0c
Erik Barroso <ebarroso@umich.edu>
2020-03-05 14:37:34 -0500

    creates welcome index and controller, sets it as root

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
   resources :orders
   resources :deliveries
   resources :locations
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  #groot the root!
+  root to: 'welcome#index'
 end
