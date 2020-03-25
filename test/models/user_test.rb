52b4b3809acb4e4c27e69df00ac0caaad99e9e6d
Derek Bruckner <dbrucknr@umich.edu>
2020-03-25 09:16:18 -0400

    Added some layout changes

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(first_name: "First", last_name: "Last",
+    email: "user@example.com", password: "password")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
 end
